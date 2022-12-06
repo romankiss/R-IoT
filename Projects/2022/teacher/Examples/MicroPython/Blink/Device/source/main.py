32e7a89782da75385f4b68e99762167be52380db
JozefDragula <74078394+JozefDragula@users.noreply.github.com>
2022-12-06 13:34:46 +0100

    Update main.py
    Test for DLD GitHub ->Thonny

--- Projects/2022/teacher/Examples/MicroPython/Blink/Device/source/main.py
+++ Projects/2022/teacher/Examples/MicroPython/Blink/Device/source/main.py
@@ -4,14 +4,16 @@ import neopixel
 import random
 
 button1 = Pin(22, Pin.IN, Pin.PULL_UP)
-
+---
+Pokus
+---
 np = neopixel.NeoPixel(machine.Pin(27), 1)
 np[0] = (0, 128, 0) # RGB  set to red, 1/2 brightness
 np.write()
 
 def doit(t):
     print("Button has been pressed!")
-    np[0] = (random.randrange(64),random.randrange(255),random.randrange(255))
+    np[0] = (random.randrange(64),random.randrange(255),random.randrange(255))  
     np.write()   
 
 button1.irq(trigger=Pin.IRQ_FALLING, handler=doit)
