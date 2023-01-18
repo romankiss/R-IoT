b02bf1539322d03ef1b864afe9debde6cb6b886d
Marek-maker <59760649+Marek-maker@users.noreply.github.com>
2023-01-18 16:06:08 +0100

    Meranie vzdialenosti
    Meranie vzdialenosti od 2 cm do 4 m pomocou snímača HC-SR04 a ESP32 Stamp C3 Mikrokontroléru.

---
+++ Projects/2023/Merák_vzdialenosti/main.py
@@ -0,0 +1,32 @@
+from hcsr04 import HCSR04
+import neopixel
+import time
+import machine
+
+np = neopixel.NeoPixel(machine.Pin(2), 1)
+np[0] = (0, 255, 0) # RGB  set green to max brightness
+np.write()
+
+sensor = HCSR04(trigger_pin=5, echo_pin=4)
+distance = 0
+
+time.sleep_ms(1000)
+
+while True:
+    
+    distance = sensor.distance_cm()
+    
+    if distance < 10:
+        np[0] = (255, 0, 0)#ak je predmet bližšie ako 10cm rozsvieti sa červená
+        np.write()
+    else:
+        np[0] = (0, 255, 0)#ak je predmet 10cm a viac od snímača rozsvieti sa zelená
+        np.write()
+        
+    print(distance)#meranie sa vykonáva každú polsekundu a vzdialenosť sa vypisuje cez Tx do konzoly
+    time.sleep_ms(500)
+        
+
+
+
+
