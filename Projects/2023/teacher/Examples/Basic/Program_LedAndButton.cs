531ca7e44a803a310b2a3eec8ead1519c525ed9a
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2023-11-20 16:13:06 +0100

    Update Program_LedAndButton.cs

--- Projects/2023/teacher/Examples/Basic/Program_LedAndButton.cs
+++ Projects/2023/teacher/Examples/Basic/Program_LedAndButton.cs
@@ -1,1 +1,41 @@
+using Iot.Device.Button;
+using Iot.Device.Ws28xx.Esp32;
+using nanoFramework.AtomLite;
+using System;
+using System.Diagnostics;
+using System.Threading;
 
+namespace NFAppAtomLite_Testing
+{
+    public class Program
+    {
+        static Sk6812 neo = null;
+        static int ii = 0;
+
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            GpioButton button = new(39, debounceTime: TimeSpan.FromMilliseconds(200));
+            button.Press += Button_Press;
+            neo = AtomLite.NeoPixel;            
+            neo.Image.SetPixel(0, 0, 0, 0, 10);
+            neo.Update();
+    
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void Button_Press(object sender, EventArgs e)
+        {
+            int i = ii++ & 0x3;
+            Debug.WriteLine($"Button has been pressed, rgb = {i}");
+            neo.Image.SetPixel(0, 0, (byte)(i==1?10:0), (byte)(i == 2 ? 10 : 0), (byte)(i == 3 ? 10 : 0));
+            neo.Update();
+        }
+    }
+}
+
+
+//neo = new Sk6812(22, 1);
+//neo.Image.SetPixel(0, 0, 0, (byte)(new Random()).Next(10), 0);
+//neo.Update();
