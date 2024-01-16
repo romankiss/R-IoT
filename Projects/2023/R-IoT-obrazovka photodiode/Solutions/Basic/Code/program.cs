af4bfa5efd60da5995d1ed3880428f8589e6ea82
pstrejr1907 <92391704+pstrejr1907@users.noreply.github.com>
2024-01-16 12:31:17 +0100

    petrik smajlik smaklik smajlik

---
+++ Projects/2023/R-IoT-obrazovka photodiode/Solutions/Basic/Code/program.cs
@@ -0,0 +1,67 @@
+using nanoFramework.Hardware.Esp32;
+using System.Device.I2c;
+using System.Threading;
+using Iot.Device.Bh1750fvi;
+
+
+using Iot.Device.Ssd13xx;
+using System.Diagnostics;
+using System;
+
+
+
+// https://docs.nanoframework.net/devicesdetails/Mfrc522/README.html
+// https://github.com/nanoframework/nanoFramework.IoT.Device/blob/develop/devices/Mfrc522/samples/Program.cs
+
+namespace NFAppAtomLite_Testing
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+
+
+
+            Configuration.SetPinFunction(21, DeviceFunction.I2C1_CLOCK);   // Grove connector
+            Configuration.SetPinFunction(25, DeviceFunction.I2C1_DATA);    // Grove connector
+            //
+            I2cDevice i2c_oled128x64 = I2cDevice.Create(new I2cConnectionSettings(1, 0x3C));
+            var display = new Iot.Device.Ssd13xx.Ssd1306(i2c_oled128x64);
+            display.Orientation = DisplayOrientation.Landscape180;
+            display.ClearScreen();
+            display.Font = new Sinclair8x8();
+
+
+
+            Configuration.SetPinFunction(22, DeviceFunction.I2C2_DATA);
+            Configuration.SetPinFunction(19, DeviceFunction.I2C2_CLOCK);
+
+            I2cConnectionSettings settings = new I2cConnectionSettings(busId: 2, (int)I2cAddress.AddPinLow);
+            I2cDevice device = I2cDevice.Create(settings);
+            Bh1750fvi sensor = new Bh1750fvi(device);
+
+            while (true)
+            {
+
+                double illuminance = sensor.Illuminance.Value;
+
+
+
+
+
+                // read illuminance(Lux)
+                Debug.WriteLine(illuminance.ToString());
+                display.ClearScreen();
+                display.Write(0, 0, illuminance.ToString());
+                display.Display();
+
+
+
+                Thread.Sleep(200);
+            }
+        }
+
+    }
+}
+
---
+++ Projects/2023/R-IoT-obrazovka photodiode/Solutions/Basic/Hardware/Controller/README.md
@@ -0,0 +1,31 @@
+**References:**
+
+<p style="display:inline-block;">
+  <img  width="10%" src="https://github.com/romankiss/R-IoT/assets/30365471/40ae9537-c80b-49c5-803d-bfe5a858fb83">
+  <a href="https://shop.m5stack.com/collections/m5-controllers/products/atom-lite-esp32-development-kit">ATOM Lite ESP32 IoT Development Kit.</a>
+  <br/><br/>
+ <img width="10%" src="https://github.com/romankiss/R-IoT/assets/30365471/dab71889-6844-4671-a300-f376cfcb8c7f">
+   <a href="https://shop.m5stack.com/products/atomic-portabc-extension-base"> ATOMIC PortABC Extension Base</a>
+  <br/><br/>
+  <img width="10%" src="https://github.com/romankiss/R-IoT/assets/30365471/52fe0b5a-8dcf-41d2-8f07-c35b4c9451d5">
+   <a href="https://docs.m5stack.com/en/unit/AtomPortABC"> Example of usage</a>
+  <br/><br/>
+</p>
+
+
+<br/><br/><br/><br/>
+
+![image](https://github.com/romankiss/R-IoT/assets/30365471/cebffa4c-8338-4030-a1e4-8667eed18f91)
+
+<br/>
+<br/>
+
+
+![image](https://github.com/romankiss/R-IoT/assets/30365471/10891be1-0619-4700-9b1e-aa733e7c5b4a)
+
+<br/>
+<br/>
+
+![image](https://github.com/romankiss/R-IoT/assets/30365471/0de638dc-9079-4741-b5ac-0011a7cef145)
+
+
