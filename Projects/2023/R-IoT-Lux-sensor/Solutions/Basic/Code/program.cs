8fdfc90a15c81a4902b026dc5d2cfe3ec8cd904f
unknown <matejvyskocil97@gmail.com>
2024-01-22 18:19:33 +0100

    Oprava

--- Projects/2023/R-IoT-Lux-sensor/Solutions/Basic/Code/program.cs
+++ Projects/2023/R-IoT-Lux-sensor/Solutions/Basic/Code/program.cs
@@ -6,6 +6,7 @@ using nanoFramework.Hardware.Esp32;
 using Iot.Device.Bh1750fvi;
 using Iot.Device.Ssd13xx;
 using Iot.Device.Button;
+using System;
 
 namespace Projekt_26._12._2023
 {
@@ -13,6 +14,8 @@ namespace Projekt_26._12._2023
     {
         public static void Main()
         {
+            double illuminance;
+            GpioButton button = new GpioButton(buttonPin: 39 , debounceTime:TimeSpan.FromMilliseconds(200));
 
             Debug.WriteLine("Hello from nanoFramework!");
             Configuration.SetPinFunction(23, DeviceFunction.I2C1_DATA);
@@ -21,28 +24,21 @@ namespace Projekt_26._12._2023
             Configuration.SetPinFunction(21, DeviceFunction.I2C2_CLOCK);
             Configuration.SetPinFunction(25, DeviceFunction.I2C2_DATA);
 
-            
-
-            GpioButton button = new GpioButton(buttonPin: 39);
-
-
             I2cConnectionSettings settings = new I2cConnectionSettings(busId: 1, (int)I2cAddress.AddPinLow);
             I2cDevice device = I2cDevice.Create(settings);
 
+
+            I2cDevice i2c_oled128x64 = I2cDevice.Create(new I2cConnectionSettings(2, 0x3C));
+            var display = new Iot.Device.Ssd13xx.Ssd1306(i2c_oled128x64);
             var sensor = new Bh1750fvi(device);
-            //read illuminance(Lux)
+
+            display.Font = new Sinclair8x8();
+            display.ClearScreen();
 
             button.Press += (sender, e) =>
             {
-                double illuminance = sensor.Illuminance.Value;
+                illuminance = sensor.Illuminance.Value; //read illuminance(Lux)
                 Debug.WriteLine($"LUX = {illuminance}");
-
-
-                I2cDevice i2c_oled128x64 = I2cDevice.Create(new I2cConnectionSettings(2, 0x3C));
-                var display = new Iot.Device.Ssd13xx.Ssd1306(i2c_oled128x64);
-
-                display.ClearScreen();
-                display.Font = new Sinclair8x8();
                 display.Write(0, 5, $"LUX = {illuminance}");
                 display.DrawHorizontalLine(0, 50, 127);
                 display.Display();
@@ -56,7 +52,6 @@ namespace Projekt_26._12._2023
             // Check our documentation online: https://docs.nanoframework.net/
             // Join our lively Discord community: https://discord.gg/gCyBu8T
         }
-
+        
     }
 }
-
