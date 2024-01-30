0eead3fe76e2070456e5728ee52eafa6d2e0852a
pstrejr1907 <92391704+pstrejr1907@users.noreply.github.com>
2024-01-30 12:49:56 +0100

    Update program.cs
    update m/w3 + led indikatore

--- Projects/2023/R-IoT-obrazovka photodiode/Solutions/Basic/Code/program.cs
+++ Projects/2023/R-IoT-obrazovka photodiode/Solutions/Basic/Code/program.cs
@@ -6,15 +6,18 @@ using Iot.Device.Bh1750fvi;
 using Iot.Device.Ssd13xx;
 using Iot.Device.Button;
 using System;
-using UnitsNet.Units;
-using UnitsNet;
+using Iot.Device.Ws28xx.Esp32;
 
 namespace NFAppAtomLite_Testing
 {
     public class Program
     {
+
+        static Sk6812 neo = null;
+
         public static void Main()
         {
+            Debug.WriteLine("not here , kein problem ");
             double illuminance;
             GpioButton button = new GpioButton(buttonPin: 39, debounceTime: TimeSpan.FromMilliseconds(200));
 
@@ -26,15 +29,16 @@ namespace NFAppAtomLite_Testing
             //display
             Configuration.SetPinFunction(22, DeviceFunction.I2C2_DATA);
             Configuration.SetPinFunction(19, DeviceFunction.I2C2_CLOCK);
-            
+            //ledka
+            Configuration.SetPinFunction(32, DeviceFunction.I2C1_DATA);
+            Configuration.SetPinFunction(26, DeviceFunction.I2C1_CLOCK);
+
             I2cDevice i2c_oled128x64 = I2cDevice.Create(new I2cConnectionSettings(2, 0x3C));
             var display = new Iot.Device.Ssd13xx.Ssd1306(i2c_oled128x64);
             var sensor = new Bh1750fvi(device);
 
-            
-           
 
-
+            var neo = new Sk6812(26, 8);
 
 
             //font do displaya
@@ -43,7 +47,7 @@ namespace NFAppAtomLite_Testing
 
             button.Press += (sender, e) =>
             {
-                
+
                 illuminance = sensor.Illuminance.Value; //read illuminance(Lux)
                 Debug.WriteLine($"LUX = {illuminance}");
                 //zaokruhlenie na cele cislo 
@@ -51,17 +55,73 @@ namespace NFAppAtomLite_Testing
 
                 display.Write(0, 5, $"LUX = {y}");
                 display.DrawHorizontalLine(0, 50, 127);
-               
+
                 display.Display();
-                
+
+                for (int n = -1; n <= y; n++)
+                {
+                    if (n == 0)
+                    {
+                        neo.Image.SetPixel(7, 0, 10, 0, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+
+
+
+
+                    if (n > 0 && n < 10)
+                    {
+                        neo.Image.SetPixel(0, 0, 0, 10, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+
+                    if (n >= 10 && n < 20)
+                    {
+                        neo.Image.SetPixel(1, 0, 0, 10, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+
+                    if (n >= 20 && n < 30)
+                    {
+                        neo.Image.SetPixel(2, 0, 0, 10, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+
+                    if (n >= 30 && n < 40)
+                    {
+                        neo.Image.SetPixel(3, 0, 0, 10, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+
+                    if (n >= 40 && n < 50)
+                    {
+                        neo.Image.SetPixel(4, 0, 0, 10, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+
+                    if (n >= 50)
+                    {
+                        neo.Image.SetPixel(5, 0, 0, 10, 0);
+                        neo.Update();
+                        Thread.Sleep(50);
+                    }
+                }
+
             };
 
 
 
-            Thread.Sleep(Timeout.Infinite);
+          Thread.Sleep(Timeout.Infinite);
 
-          
+
+        
+            
         }
-
     }
 }
