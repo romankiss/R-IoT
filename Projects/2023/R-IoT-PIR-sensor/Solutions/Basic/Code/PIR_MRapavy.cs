bb3a6035003114a249c0b57d3359e1c79f378bd9
MarkusiTV <121643952+MarkusiTV@users.noreply.github.com>
2024-02-26 09:51:13 +0000

    Rename Program.cs to PIR_VKocian.cs and added PIR_MRapavy.cs

---
+++ Projects/2023/R-IoT-PIR-sensor/Solutions/Basic/Code/PIR_MRapavy.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Iot.Device.Hcsr501;
+using System.Device.Gpio;
+
+
+namespace PIR
+{
+
+    // https://docs.nanoframework.net/devicesdetails/Mfrc522/README.html
+    // https://github.com/nanoframework/nanoFramework.IoT.Device/blob/develop/devices/Mfrc522/samples/Program.cs
+
+    public class Program
+    {
+        static Hcsr501 sensorPIR = null;
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+        #region PIR
+            sensorPIR = new Hcsr501(25);    //ATOMLITE HAT - 25 (I2C OUT)
+            //sensorPIR.IsMotionDetected += (s, e) =>
+            sensorPIR.Hcsr501ValueChanged += Motion;
+
+            /*
+            {
+                Debug.WriteLine("test pohyb");
+                if (e.PinValue == PinValue.High)
+                {
+                    Debug.WriteLine("pohyb");
+                }
+            };
+            */
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        private static void Motion(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Pohyb!");
+            if (true)
+            {
+                Debug.WriteLine("Pohyb");
+            }
+        }
+        #endregion
+    }
+}
