2a518abe03d8f33e6753750c2ebd1a97fc6a92a2
Artemis <117931806+Arty1s@users.noreply.github.com>
2024-03-25 10:00:38 +0100

    Code update 1.0

--- Projects/2023/R-IoT-Sound-Control/Solutions/Basic/Code/program.cs
+++ Projects/2023/R-IoT-Sound-Control/Solutions/Basic/Code/program.cs
@@ -1,1 +1,37 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Device.Gpio;
+//using System.Device.Adc;
+//using Windows.Devices.Adc;
 
+
+namespace Sound
+{
+    public class Program
+    {
+        static GpioController gpio_controller;
+        static GpioPin digital_pin;
+        //static GpioPin analog_pin;
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            gpio_controller = new GpioController();
+            digital_pin = gpio_controller.OpenPin(25, PinMode.Input);
+
+            /*AdcController adc = AdcController.GetDefault();
+            AdcChannel ac4 = adc.OpenChannel(0);*/
+            //analog_pin = gpio_controller.OpenPin(21, PinMode.Input);
+
+            while (true)
+            {
+                Debug.WriteLine($"Digital val.: {digital_pin.Read()}");
+                //Debug.WriteLine($"Analog val.: {ac4.ReadValue()}");
+                Thread.Sleep( 100 );
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
