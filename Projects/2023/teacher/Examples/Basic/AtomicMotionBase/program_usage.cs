19ef6dae0142c1d10ab78731d7a27f507c4a25be
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2024-05-06 16:35:15 +0200

    Update program_usage.cs

--- Projects/2023/teacher/Examples/Basic/AtomicMotionBase/program_usage.cs
+++ Projects/2023/teacher/Examples/Basic/AtomicMotionBase/program_usage.cs
@@ -1,4 +1,3 @@
-#region Servo Atomic Motion Base           
-servo = M5AtomicMotion.Create(busId:2);
-Debug.WriteLine($"Servo busId = {(servo == null ? 0 : servo.I2cDevice.ConnectionSettings.BusId)}");
-#endregion
+
+  var servo = M5AtomicMotion.Create(busId:2);
+
