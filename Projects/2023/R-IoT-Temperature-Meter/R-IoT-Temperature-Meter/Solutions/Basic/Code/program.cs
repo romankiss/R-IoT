2ed533a3f7f2bab7a161a252bf7edaacdcf22284
Jozef Dragula <patrik.balaz227@gmail.com>
2024-05-14 10:32:06 +0200

    pato

--- Projects/2023/R-IoT-Temperature-Meter/R-IoT-Temperature-Meter/Solutions/Basic/Code/program.cs
+++ Projects/2023/R-IoT-Temperature-Meter/R-IoT-Temperature-Meter/Solutions/Basic/Code/program.cs
@@ -1,105 +1,111 @@
-using Iot.Device.Button;
-using Iot.Device.Ws28xx.Esp32;
-using Iot.Device.Sht3x;
-using nanoFramework.Hardware.Esp32;
 using System;
-using System.Device.I2c;
 using System.Diagnostics;
+using System.Net;
 using System.Threading;
+using nanoFramework.WebServer;
+using nanoFramework.Networking;
+using System.Net.NetworkInformation;
+using System.IO;
+using nanoFramework.Hardware.Esp32;
 
 
-namespace NFAppAtomLite_Testing
+
+namespace WebServerTest
 {
-    public class Program
+    public class Wifi
     {
-
-        static Sk6812 neo = null;
-
-
-        public static void Main()
+        static string _ssid = null;
+        static string _password = null;
+        static string _telemetrydataFilePath = null;
+        public Wifi(string ssid, string password, string telemetrydataFilePath)
         {
+            _ssid = ssid;
+            _password = password;
+            _telemetrydataFilePath = telemetrydataFilePath;
 
-            GpioButton button = new GpioButton(buttonPin: 39);
+            WIficonnect();
 
-            Debug.WriteLine("Hello from nanoFramework!");
+        }
 
-            var neo = new Sk6812(26, 8);
+        public static void WIficonnect()
+        {
+            try
+            {
+                CancellationTokenSource cs = new(90000);
+                Debug.WriteLine($"Connecting to {_ssid} network ...");
+                var success = WifiNetworkHelper.ScanAndConnectDhcp(_ssid, _password, requiresDateTime: true, token: cs.Token);
+                if (success)
+                {
+                    //Blink(0, 10, 0, 100, 2);
+                    Debug.WriteLine($"Wifi ready - {_ssid}, mac:{GetMacId(out string ipaddress)}, ip:{ipaddress}");
+                    // Otherwise, you are connected and have a valid IP and date
+                    Debug.WriteLine($" {WifiNetworkHelper.Status}");
 
+                    WebServer server = new WebServer(80, HttpProtocol.Http);
 
-            button.Press += (sender, e) =>
+                    // Add a handler for commands that are received by the server.
+                    server.CommandReceived += ServerCommandReceived;
+
+                    // Start the server.
+                    server.Start();
+                }
+                else
+                {
+                    Debug.WriteLine($"Can't connect to the network {_ssid}, error: {WifiNetworkHelper.Status}");
+                    throw new Exception($"Wifi is not connected.");
+                }
+            }
+            catch (Exception ex)
             {
 
-                #region Temp&Hum  - sensor sht30   (RK: configuraciu pinov, sensorov, atd. treba urbot mimo "event handler", lebo ma byt vykonana len raz a nie pri kazdej udalosti)
-                Configuration.SetPinFunction(32, DeviceFunction.I2C1_CLOCK);
-                Configuration.SetPinFunction(26, DeviceFunction.I2C1_DATA);
-                Sht3x sensorTH = new(new(new I2cConnectionSettings(1, 0x44)));
-                Debug.WriteLine($"sensorTH: temperature[C]={sensorTH.Temperature.DegreesCelsius:F2}, humidity[%]={sensorTH.Humidity.Percent:F2}");
-                #endregion
-
-
-                int i = ((int)sensorTH.Temperature.DegreesCelsius);
-
-                for (int n = -10; n <= i; n++)
-                {
-                    if (n < -5)
-                    {
-                        neo.Image.SetPixel(0, 0, 0, 0, 10);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= -5 && n < 0)
-                    {
-                        neo.Image.SetPixel(1, 0, 0, 0, 10);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= 0 && n < 5)
-                    {
-                        neo.Image.SetPixel(2, 0, 10, 0, 0);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= 5 && n < 10)
-                    {
-                        neo.Image.SetPixel(3, 0, 10, 0, 0);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= 10 && n < 15)
-                    {
-                        neo.Image.SetPixel(4, 0, 10, 0, 0);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= 15 && n < 20)
-                    {
-                        neo.Image.SetPixel(5, 0, 10, 0, 0);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= 20 && n < 25)
-                    {
-                        neo.Image.SetPixel(6, 0, 10, 0, 0);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-
-                    if (n >= 25)
-                    {
-                        neo.Image.SetPixel(7, 0, 10, 0, 0);
-                        neo.Update();
-                        Thread.Sleep(50);
-                    }
-                }
-            };
-
-            Thread.Sleep(Timeout.Infinite);
+            }
         }
+
+        private static void ServerCommandReceived(object source, WebServerEventArgs e)
+        {
+            var url = e.Context.Request.RawUrl;
+            Debug.WriteLine($"Command received: {url}, Method: {e.Context.Request.HttpMethod}");
+
+            if (url.ToLower() == "/sayhello")
+            {
+                //Under construction
+                string body = File.ReadAllText(_telemetrydataFilePath);
+                WebServer.OutPutStream(e.Context.Response, "<!DOCTYPE html> <html><head>" +
+"<title>Hi from nanoFramework Server</title></head><body> <br><p id=\"demo\"></p><p>" + body + "</p></body>" +
+"<script>let text = " + body + ";\r\nconst myArray = text.split();\r\n\r\ndocument.getElementById(\"demo\").innerHTML = myArray;\r\n</script></html>");
+            }
+            else
+            {
+                WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.NotFound);
+            }
+        }
+
+        #region GetMacId
+        static string GetMacId(out string ipaddress)
+        {
+            NetworkInterface[] nis = NetworkInterface.GetAllNetworkInterfaces();
+            if (nis.Length > 0)
+            {
+                // get the first interface
+                NetworkInterface ni = nis[0];
+                ipaddress = ni.IPv4Address;
+                return ByteArrayToHex(ni.PhysicalAddress);
+            }
+            else
+            {
+                ipaddress = string.Empty;
+                return "000000000000";
+            }
+        }
+        static string ByteArrayToHex(byte[] barray)
+        {
+            string bs = "";
+            for (int i = 0; i < barray.Length; ++i)
+            {
+                bs += barray[i].ToString("X2");
+            }
+            return bs;
+        }
+        #endregion
     }
 }
