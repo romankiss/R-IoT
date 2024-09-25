9555fedb0c4d87e4c12b7e6c12eec5ef1d90c5d5
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2024-09-25 14:25:00 +0200

    Update samples.cs

--- Projects/2024/PatternsAndPractices/Delegates/samples.cs
+++ Projects/2024/PatternsAndPractices/Delegates/samples.cs
@@ -1,1 +1,62 @@
+// Mastering Delegates and Events In C# .NetFramework
+// https://www.c-sharpcorner.com/UploadFile/84c85b/delegates-and-events-C-Sharp-net/
+//
+// Events
+
+// declaration of delegate
+public delegate void OnTouchEvent(object source, OnTouchEventArgs e);
+
+// interesting event
+public class OnTouchEventArgs : EventArgs
+{
+    public ushort X { get; set; }
+    public ushort Y { get; set; } 
+    // ... add more based on the needs
+}
+
+// my driver
+public class MyDriver 
+{
+    public event OnTouchEvent OnChangeValue = null;
+
+    public MyDriver() 
+    {
+      //...
+      var task = new Thread(() =>
+      {
+        while(true) 
+        {
+          // polling my event, compare old and new values, store new valuse, set flag IsThereChange
+          ...
+          if(OnChangeValue != null && IsThereChange)
+          {
+            var args = new OnTouchEventArgs() {X = newX, Y=newY};
+            OnChangeValue.Invoke(this, args);
+          }
+          Thread.Sleep(1000);
+        }
+            
+      })
+      { Priority = ThreadPriority.BelowNormal };
+      task.Start();      
+    }
+
+}
+
+
+
+// usage of events in the application
+MyDriver mydriver = new MyDriver();
+if(mydriver != null)
+{
+  mydriver.OnChangeValue += (sender, e) =>
+  {
+    // to do
+  }
+
+  
+}
+
+
+
 
