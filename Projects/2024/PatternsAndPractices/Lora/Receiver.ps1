0b14f5d18e4632985e5e1605a18825995d80f7f4
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2024-09-26 00:07:04 +0200

    Update Receiver.ps1

--- Projects/2024/PatternsAndPractices/Lora/Receiver.ps1
+++ Projects/2024/PatternsAndPractices/Lora/Receiver.ps1
@@ -1,1 +1,26 @@
+#Lora Read packet via RYLR998 module using AT commands
+function readlora {
+    $counter = 0
+    $COM = [System.IO.Ports.SerialPort]::getportnames()
+    $port= new-Object System.IO.Ports.SerialPort $COM,115200,None,8,one
+    $port.ReadTimeout = 5000
+    $port.Open()
+    $port.Write("AT" + "`r`n")
+    $port.ReadLine()
+    $port.Write("AT+RESET" + "`r`n")
+    $port.ReadLine()
+    #$port.Write("AT+BAND=868500000,M" + "`r`n")
+    #$port.ReadLine()
+    #$port.Write("AT+ADDRESS?" + "`r`n")
+    #$port.ReadLine()
 
+    do {
+            
+        $line = $port.ReadLine()
+        Write-Host "$($line)" 
+                    
+    }
+    while ($port.IsOpen)
+    $port.Close()
+    $port.Dispose()
+}
