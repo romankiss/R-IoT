120fd51f80ddc78e6c7981793d0fa5ae8eb88049
Marek-maker <59760649+Marek-maker@users.noreply.github.com>
2024-11-12 20:18:16 +0100

    Json workaround

--- Projects/2024/R-IoT-Data_Hub/Solutions/Basic/Json_converter/program.cs
+++ Projects/2024/R-IoT-Data_Hub/Solutions/Basic/Json_converter/program.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
+//Demonštrácia konverzie veličín a ich hodnôt z dát. typu struct na Json formát a naopak (využiteľné pri LoRa prenosoch)
+//funkcie nemajú skoro žiadne ošetrenie pred neočakávanými hodnotami, takže iné ako dobré vstupy spôsobia chyby/nepredvídateľné chovanie
+
 
 namespace Json_LoRa_Msg
 {
-    //create a struct that has public fields so that it can be converted to json
+
     public struct SensorData { public double Temperature; public double Humidity; }
 
     public static class SimpleJsonConverter
@@ -12,22 +15,59 @@ namespace Json_LoRa_Msg
         public static string SerializeObject(SensorData obj)
         {
             //if (obj ==null) { throw new ArgumentNullException(nameof(obj)); }
-            //every valid json starts with { and ends with }
             var json = "{";
-            var properties = obj.GetType().GetFields();//creates a list of the properties / fields of the given struct/class/object
+            var properties = obj.GetType().GetFields();
 
             for (int i = 0; i < properties.Length; i++)
             {
                 var property = properties[i];
-                var name = property.Name;//gets the name
-                var value = property.GetValue(obj);//gets the val
-                json += "\"" + name + "\": " + value;//formats the name and its value to a json format
-                if (i < properties.Length - 1) { json+=", "; }//if there will be some more fields a ", " is added
+                var name = property.Name;
+                var value = property.GetValue(obj);
+                json += "\"" + name + "\": " + value;
+                if (i < properties.Length - 1) { json+=", "; }
             }
             json += "}";
             return json;
         }
 
+
+        public static SensorData ParseJson(string json)
+        {
+            SensorData data = new SensorData();
+            // Odstránenie zbytočných znakov
+            json = json.Trim('{', '}');
+            json = RemoveCharacters(json, new char[] { '\"' });// napr.  "temp" -> temp
+            // Rozdelenie na jednotlivé páry kľúč-hodnota
+            string[] pairs = json.Split(',');
+            foreach (var pair in pairs)
+            {
+                string[] keyValue = pair.Split(':');
+                string key = keyValue[0].Trim();
+                string value = keyValue[1].Trim();
+                // Priradenie hodnôt k vlastnostiam štruktúry
+                if (key.Equals("Temperature"))
+                {
+                    data.Temperature = double.Parse(value);
+                }
+                else if (key.Equals("Humidity")) 
+                {
+                    data.Humidity = double.Parse(value); 
+                }
+            }
+            return data;
+
+        }
+
+        public static string RemoveCharacters(string input, char[] charsToRemove) { 
+            string result = "";
+            foreach (char c in input) {
+                bool shouldRemove = false; 
+                foreach (char remove in charsToRemove) { 
+                    if (c == remove) { shouldRemove = true; break; }
+                }
+                if (!shouldRemove) { result += c; }
+            } return result; 
+        }
     }
     public class Program
     {
@@ -37,11 +77,17 @@ namespace Json_LoRa_Msg
 
            
             // Vytvorenie objektu s nameranými hodnotami
-            SensorData data = new SensorData { Temperature = 23.5, Humidity = 60.2 };//creates an object from the struct and assigns some values to its properties
+            SensorData data = new SensorData { Temperature = 23.5, Humidity = 60.2 };
             // Konverzia objektu do formátu JSON
-            string json = SimpleJsonConverter.SerializeObject(data);//converts the above created obj to a json format of a string
+            string json = SimpleJsonConverter.SerializeObject(data);
             // Výpis JSON reťazca
+            Console.WriteLine("We have imputed the SensorData struct, with the vals: temperature: " + data.Temperature + ", humidity: " + data.Humidity);
+            Console.WriteLine("This is the Json it produced: ");
             Console.WriteLine(json);
+
+            SensorData check_data = SimpleJsonConverter.ParseJson(json);
+            Console.WriteLine("And now we have given the above Json, the following was extracted from it: temperature: " + check_data.Temperature + ", humidity: " + check_data.Humidity);
+            
 
 
             Thread.Sleep(Timeout.Infinite);
