39f542890ca549b54f99fbd4e3545a3381cc60bb
Marek-maker <59760649+Marek-maker@users.noreply.github.com>
2024-11-25 20:06:00 +0100

    Okomentovanie pre potreby RPJ

--- Projects/2024/R-IoT-Data_Hub/Solutions/Basic/Dummy_LoRa_Node/Blink.cs
+++ Projects/2024/R-IoT-Data_Hub/Solutions/Basic/Dummy_LoRa_Node/Blink.cs
@@ -1,44 +1,42 @@
+/*Trieda pre jednoduché blikanie trojfarebnou LEDkou vo veľajšom vlákne.*/
 using System;
 using System.Diagnostics;
 using System.Threading;
+using Iot.Device.Ws28xx.Esp32;//ovládanie LED
 
-using Iot.Device.Ws28xx.Esp32;
-
-
-namespace LoRa
+namespace LoRa//zmeniť podľa potreby
 {
     internal class Blink
     {
-        static Sk6812 neo = new Sk6812(35, 1);
-
+        static Sk6812 neo = null;//vytvorenie objektu s názvom neo, ktorý je triedy Sk6812
         public Blink()
         {
-            neo = new Sk6812(35, 1);
-
+            neo = new Sk6812(35, 1);//konštruktor triedy Blink, priradenie ovládaču 35. GPIO pin na ktorom je LED
         }
-
 
         #region Blink
         public static void Blinks(byte r, byte g, byte b, int periodIntMs = 100, double duty = 0.5, int times = 1)
         {
-            var Blink_thread = new Thread(() => {
-                for (int time = times; time > 0; --time)
+            /* Vysvetlenie parametrov:
+             * r, g, b: farebný kód (0-255);
+             * periodIntMs: perióda blikania v ms;
+             * duty: podiel zapnutého stavu LED počas 1 periódy;
+             * times: počet opakovaní bliknutia;
+             */
+            var Blink_thread = new Thread(() => {//definíca nového vlákna
+                for (int i = times; i > 0; --i)
                 {
                     neo.Image.SetPixel(0, 0, r, g, b);
                     neo.Update();
                     Thread.Sleep((int)(periodIntMs * duty));
                     neo.Image.Clear();
                     neo.Update();
-                    if (time > 1) Thread.Sleep((int)(periodIntMs * (1 - duty)));
+                    if (i > 1) Thread.Sleep((int)(periodIntMs * (1 - duty)));
                 }
             })
             { Priority = ThreadPriority.BelowNormal };
-            Blink_thread.Start();
+            Blink_thread.Start();//spustenie vlákna, v ktorom bežia súbežne s hlavným vláknom operácie vykonávajúce efekt blikania LED
         }
         #endregion
-
-
-
-
     }
 }
