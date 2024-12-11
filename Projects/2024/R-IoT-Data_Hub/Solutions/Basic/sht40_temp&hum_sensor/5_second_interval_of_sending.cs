671b27204135845136b46f54f28b26d550890ae5
Marek-maker <59760649+Marek-maker@users.noreply.github.com>
2024-12-11 21:26:10 +0100

    a bit better implementation

--- Projects/2024/R-IoT-Data_Hub/Solutions/Basic/sht40_temp&hum_sensor/5_second_interval_of_sending.cs
+++ Projects/2024/R-IoT-Data_Hub/Solutions/Basic/sht40_temp&hum_sensor/5_second_interval_of_sending.cs
@@ -7,17 +7,48 @@ using nanoFramework.Json;
 using System.Device.I2c;
 using Iot.Device.Sht4x;
 using System.Text;
+using Iot.Device.Ws28xx.Esp32;
+
+/*prerobiť na event driven
+pre-processing
+processing
+post-processing
+
+timer envoke handler
+lora async --- potálto ok
+---odtálto sa bude robiť
+BATCH publishing ... uložiť do logov samples with timestamp(short) a poslať naraz (sampling každú0,5s, publishing každých napr 5s)
+
+ optimalizácie prenosu: 
+byte/bit precision coding
+temp nemusí byť ani int32, ani byte8, ale napr polka bytu (4b) na temp (16 možných rôznych hodnót - je jasné a predpokladané, že budú v rozmedí od napr. 10 do 35 stupňov)
+ */
 
 
 namespace DOD
 {
     public class SensorData
     {
-        public float Temp { get; set; }
-        public float Hum { get; set; }
+        public int Temp { get; set; }
+        public int Hum { get; set; }
     }
+
+    //work in progress
+    public class Packet
+    {
+        public int counter { get; set; } //takes 4B
+        public DateTime sendTime { get; set; }//idk compress somehow (h:min:sec 24*60*60)
+        public SensorData payload { get; set; }//temp takes B/2, hum 1B (6b for vals eg 30 - 94 and 2b for precision of 1/4 %)
+    }
+
+
     public class Program
     {
+        
+        public static void Stream_handler()
+        {
+            Console.WriteLine("Entering Stream handler");
+        }
 
         public static void Main()
         {
@@ -25,7 +56,7 @@ namespace DOD
             Debug.WriteLine("Hello from nanoFramework!");
             //M5stack AtomS3 Lite has the button connected to pin 41, change dapending on your target platform
             GpioButton button = new GpioButton(buttonPin: 41);
-
+            Blink led = new Blink();//construct of the blink obj
             var tmp_hum = new SensorData();
 
             try
@@ -68,7 +99,7 @@ namespace DOD
                     }
                             
                     
-                    Blink.Blinks(255, 255, 255, 1000, 1, 1);
+                    led.StartBlinking(255, 255, 255, 1000, 1, 1);
                     
                     //Storage.new_formated_record(file_path, e.Data);
                 };
@@ -84,8 +115,8 @@ namespace DOD
                 try
                 {
                     data = temp_hum_meter_snsr.ReadData(MeasurementMode.NoHeaterHighPrecision);
-                    tmp_hum.Temp = (float)data.Temperature.DegreesCelsius;
-                    tmp_hum.Hum = (float)data.RelativeHumidity.Percent;
+                    tmp_hum.Temp = (int)data.Temperature.DegreesCelsius;
+                    tmp_hum.Hum = (int)data.RelativeHumidity.Percent;
                     /*Debug.WriteLine($"Temperature: {tmp_hum.Temp}\u00B0C");
                     Debug.WriteLine($"Relative humidity: {tmp_hum.Hum}%RH");*/
                 }
@@ -93,18 +124,20 @@ namespace DOD
                 var json = JsonConvert.SerializeObject(tmp_hum);
                 lora.SendAsync(0, json);
                 Debug.WriteLine(json);
-                Blink.Blinks(0, 0, 255, 1000, 0.5, 2);
+                led.StartBlinking(0, 0, 255, 1000, 0.5, 2);
             };
 
-            while (true)//this is not good, but that is how it is done fast (DOD is tomorow)
+            static TimerCallback timer_handler(Sht4X temp_hum_meter_snsr, SensorData tmp_hum, Blink led, RYLR998 lora )
             {
+                Debug.WriteLine("Hellloo");
+
                 Sht4XSensorData data = null;
 
                 try
                 {
                     data = temp_hum_meter_snsr.ReadData(MeasurementMode.NoHeaterHighPrecision);
-                    tmp_hum.Temp = (float)data.Temperature.DegreesCelsius;
-                    tmp_hum.Hum = (float)data.RelativeHumidity.Percent;
+                    tmp_hum.Temp = (int)data.Temperature.DegreesCelsius;
+                    tmp_hum.Hum = (int)data.RelativeHumidity.Percent;
                     /*Debug.WriteLine($"Temperature: {tmp_hum.Temp}\u00B0C");
                     Debug.WriteLine($"Relative humidity: {tmp_hum.Hum}%RH");*/
                 }
@@ -112,11 +145,21 @@ namespace DOD
                 var json = JsonConvert.SerializeObject(tmp_hum);
                 lora.SendAsync(0, json);
                 Debug.WriteLine(json);
-                Blink.Blinks(0, 0, 255, 1000, 0.5, 1);
-                Thread.Sleep(5000);
+                led.StartBlinking(0, 0, 255, 1000, 0.5, 2);
+
+                return null;
+            }
+            try {
+                Timer timer_watchdog = new Timer((s) => timer_handler(temp_hum_meter_snsr, tmp_hum, led, lora), null, 5000, 5000);
+            }catch(Exception ex)
+            {
+                Debug.WriteLine("EX thrwn: " + ex.Message);
             }
 
-            //Debug.WriteLine(Storage.read(file_path));
+
+
+
+
             Thread.Sleep(Timeout.Infinite);
 
             // Browse our samples repository: https://github.com/nanoframework/samples
