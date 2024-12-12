c71a17740c0e0263c4a9c5600b50008992244d85
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2024-12-12 13:42:42 +0100

    Update NeoPixel.cs
    Update code to control a NeoPixel Power pin.

--- Projects/2024/Teacher/NeoPixel.cs
+++ Projects/2024/Teacher/NeoPixel.cs
@@ -1,7 +1,8 @@
 
 // code snippet for NeoPixel (LED)
 // nuget assembly: nanoFramework.Iot.Device.Ws28xx.Esp32
-
+// nuget assembly: nanoFramework.System.Device.Gpio
+//
 // based on https://github.com/nanoframework/nanoFramework.IoT.Device/tree/develop/devices/Ws28xx.Esp32
 
 
@@ -12,17 +13,25 @@ using System.IO.Ports;
 using System.Threading;
 using Iot.Device.Ws28xx.Esp32;
 using nanoFramework.Hardware.Esp32;
+using System.Device.Gpio;
 
 // Configure the count of pixels
 const int Count = 1; 
 // Adjust the pin number, where is the NeoPixel connected. 
-const int Pin = 10;
+const int PinNeo = 20;               // NanoC6: 20
+// Adjust the pin number, where is the NeoPixel Power connected. 
+const int PinNeoPower = 19;          // NanoC6: 19
+// Create controller for Input/Output pins
+static GpioController ioctrl = new GpioController();
 
 
 //
 // usage of Ws2808 driver    
 //
-Ws28xx neo = new Ws2808(Pin, Count);
+// set the power on NeoPixel
+if (PinNeoPower >= 0) ioctrl.OpenPin(PinNeoPower, PinMode.Output).Write(PinValue.High);
+//
+Ws28xx neo = new Ws2808(PinNeo, Count);
 //neo.Image.SetPixel(0, 0, Color.Blue);
 neo.Image.SetPixel(0, 0, 10, 0, 0);
 neo.Update();
