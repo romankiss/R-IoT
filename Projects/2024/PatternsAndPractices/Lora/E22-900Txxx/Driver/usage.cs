f8af22a99a27af738684ae26c1a01f9dfdc569f4
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2024-12-15 17:08:31 +0100

    Update usage.cs

--- Projects/2024/PatternsAndPractices/Lora/E22-900Txxx/Driver/usage.cs
+++ Projects/2024/PatternsAndPractices/Lora/E22-900Txxx/Driver/usage.cs
@@ -1,1 +1,24 @@
+// code snippet
 
+static int loopback_counter = 0;
+static E22 lora = null;
+static ushort BroadcastAddress = 0xFFFF;
+
+
+Configuration.SetPinFunction(pinCOM2_TX, DeviceFunction.COM2_TX);
+Configuration.SetPinFunction(pinCOM2_RX, DeviceFunction.COM2_RX);
+lora = E22.Create("COM2", loraAddress: loraAddress);
+if (lora != null)
+{
+    lora.OnPacketReceived += (sender, e) =>
+    {
+        Debug.WriteLine($"nFmem_LoRaRcv={Memory.Run(true)}");
+        if (e.Data.StartsWith("6C6F6F706261636B"))
+        {
+            int counter = Interlocked.Increment(ref loopback_counter);
+            lora.SendAsync(e.AddressID, $"Loopback_#{counter:D4}", loraNetworkId);
+        }
+        //WriteTextOnScreen($"E22: {e.AddressID:X4} {e.Data}", X: 0, Y: LCD_Y_StatusLine, fcolor: Color.GreenYellow, maxChars: LCD_NumberOfSmallChars, font: SmallFont);
+    };
+    sensors.Append("LoRa ");
+}
