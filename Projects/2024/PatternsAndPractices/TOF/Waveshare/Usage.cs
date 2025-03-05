25684cff385990f0954e9da945b75c64e16412c8
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2025-03-05 11:54:13 +0100

    Update Usage.cs

--- Projects/2024/PatternsAndPractices/TOF/Waveshare/Usage.cs
+++ Projects/2024/PatternsAndPractices/TOF/Waveshare/Usage.cs
@@ -1,1 +1,13 @@
 
+static ToFSense sensorTOFSense = null;
+
+
+
+// Waveshare ToF Sensor
+i2c_tof = new(new I2cConnectionSettings(1, ToFSense.DefaultI2cAddress)); // Grove connector
+res = i2c_tof.WriteByte(0x07);
+if (res.Status == I2cTransferStatus.FullTransfer)
+{
+    sensorTOFSense = new ToFSense(i2c_tof);
+    Debug.WriteLine($"sensorTOF: {sensorTOFSense.Distance} mm");
+}
