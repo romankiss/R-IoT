7ffbf9d90a4edfa0f8b1966816b7a942f867223d
Marek Ratzenböck <ratzenbock.marek@gmail.com>
2025-03-10 20:05:55 +0100

    changes based on the RK hints, but not tested yet, some wirings of the i2c are still the old ones....

--- Projects/2024/R-IoT-Data_Hub/Solutions/Basic/CANSAT/Program.cs
+++ Projects/2024/R-IoT-Data_Hub/Solutions/Basic/CANSAT/Program.cs
@@ -1,4 +1,9 @@
- using System;
+#define AtomS3Lite      // XIASEEED_S3, XIASEEED_C3, AtomS3Lite, ...
+
+
+
+
+using System;
 using System.Device.Gpio;
 using System.Diagnostics;
 using System.Threading;
@@ -11,21 +16,54 @@ using Iot.Device.Vl53L0X;//for ToF sensor
 using Iot.Device.Bmxx80;//for pressure sensor
 using Iot.Device.Bmxx80.PowerMode;
 using UnitsNet; // for pressure and temperature units
+using Memory = nanoFramework.Runtime.Native.GC;
 
 namespace CanSat
 {
     public class Program
     {
 
+#if XIASEEED_S3
+        //https://wiki.seeedstudio.com/xiao_esp32s3_getting_started/
+        const string deviceId = "device2.00";
+        const ushort loraAddress = 0x905;
+        const byte loraNetworkId = 0x12;  // 850.125 + 18 = 868.125Mhz
+        const int pinButton = 0;
+        const int pinNeo = -1;
+        const int pinNeoPower = -1;
+        const int pinLedA = 21;
+        const int pinI2C1_SDA = 5;
+        const int pinI2C1_SCK = 6;
+        const int pinCOM2_TX = 43;       
+        const int pinCOM2_RX = 44;
+        const int pinCOM1_TX = 1;
+        const int pinCOM1_RX = 2;
+#elif AtomS3Lite
+        // 
+        const string deviceId = "device1.00";
+        const ushort loraAddress = 0x1234;
+        const byte loraNetworkId = 0x12;  // 850.125 + 18 = 868.125Mhz
+        const int pinButton = 41;
+        const int pinNeo = 35;
+        const int pinNeoPower = -1;
+        const int pinLedA = -1;
+        const int pinI2C1_SDA = 2;      // Grove
+        const int pinI2C1_SCK = 1;      // Grove
+        const int pinCOM2_TX = 6;       // HAT-G6, PORTC-G6,   G33, Grove-G2,  
+        const int pinCOM2_RX = 5;       // HAT-G8, PORTC-G5,   G19, Grove-G1,  
+        const int pinCOM1_TX = 8;       // PORTB-G8, 
+        const int pinCOM1_RX = 7;       // PORTB-G7
+#endif
 
         //static int loopback_counter = 0;
+        static int pub_counter = 0;
         static E22 lora = null;
-        static ushort broadcastAddress = 0xFFFF;
-        const ushort loraAddress = 0x1234;  //55????;
-        const byte loraNetworkId = 0x12;    // 850.125 + 18 = 868.125Mhz
-        static Sht4X temp_hum_meter_snsr = null;
+        static ushort BroadcastAddress = 0xFFFF;
+        static Sht4X sensorTH = null;
         static Vl53L0X sensorToF = null;
-        static Bmp280 bmp280 = null; // BMP280 sensor
+        //static ToFSense sensorToF = null;
+        static Bmp280 sensorBMP280 = null;
+        static GpioController ioctrl = new GpioController();
 
 
         public static void Main()
@@ -44,9 +82,10 @@ namespace CanSat
             // namespaces ---see upvise
 
             #region setup
+            /*remove this part later*/
             // ESP32 Hardware, board, chip, etc.
             const int pinButton = 41;
-            const int pub_period = 10000;     // miliseconds
+            
             const int pinCOM2_TX = 2;
             const int pinCOM2_RX = 1;
             const int pinI2C2_SDA = 8;
@@ -54,43 +93,40 @@ namespace CanSat
             const int pinI2C1_SDA = 6;
             const int pinI2C1_SCL = 5;
 
+            const int pub_period = 1000;     // miliseconds
+
             Blink led = new Blink();//frustrating to discover that this little line is required to init the neo obj
 
             // Button setup
             GpioButton buttonM5 = new GpioButton(buttonPin: pinButton, debounceTime: TimeSpan.FromMilliseconds(333));
+            #endregion
 
-            
 
-            try
+            #region SENSORS 
+            //ioctrl.OpenPin(pinI2C1_SDA, PinMode.InputPullUp);  
+            //ioctrl.OpenPin(pinI2C1_SCK, PinMode.InputPullUp); 
+            Configuration.SetPinFunction(pinI2C1_SDA, DeviceFunction.I2C1_DATA);
+            Configuration.SetPinFunction(pinI2C1_SCK, DeviceFunction.I2C1_CLOCK);
+
+
+            #region T&H 
+            I2cDevice i2c_th = new(new I2cConnectionSettings(1, Sht4X.I2cDefaultAddress));     // Grove connector
+            var resTH = i2c_th.WriteByte(0x07);
+            if (resTH.Status == I2cTransferStatus.FullTransfer)
             {
-                // Configure the I2C GPIOs used for the bus
-                Configuration.SetPinFunction(pinI2C2_SDA, DeviceFunction.I2C2_DATA);
-                Configuration.SetPinFunction(pinI2C2_SCL, DeviceFunction.I2C2_CLOCK);
+                sensorTH = new Sht4X(i2c_th);
+                //i2c_th.WriteByte(0x96);         //send soft reset to avoid initial CRC non-validity
+                var data = sensorTH?.ReadData(Iot.Device.Sht4x.MeasurementMode.NoHeaterMediumPrecision);
+                if (data != null)
+                {
+                    Debug.WriteLine($"sensorTH: temperature[C]={data.Temperature.DegreesCelsius:F2}, humidity[%]={data.RelativeHumidity.Percent:F2}");
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Bruh, exeptioon: " + ex.ToString());
-            }
-            I2cConnectionSettings I2C_settings = new(2, Sht4X.I2cDefaultAddress);
-            I2cDevice temp_hum_meter_device = I2cDevice.Create(I2C_settings);
-            Sht4X temp_hum_meter_snsr = new(temp_hum_meter_device);
+            #endregion
 
-
-            temp_hum_meter_device.WriteByte(0x96);//send soft reset to avoid initial CRC non-validity
-
-            try//for tof sensor
-            {
-                Debug.WriteLine("now configing");
-                Configuration.SetPinFunction(pinI2C1_SDA, DeviceFunction.I2C1_DATA);//25 6
-                Configuration.SetPinFunction(pinI2C1_SCL, DeviceFunction.I2C1_CLOCK);//21 5
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+            #region ToF
             I2cDevice i2c_tof = I2cDevice.Create(new I2cConnectionSettings(1, Vl53L0X.DefaultI2cAddress));
-            var res = i2c_tof.WriteByte(0x07);
+            var resToF = i2c_tof.WriteByte(0x07);
             sensorToF = new Vl53L0X(i2c_tof, 500);
 
             if (sensorToF != null)
@@ -103,26 +139,31 @@ namespace CanSat
             {
                 Debug.WriteLine("ToF sensor initialization failed.");
             }
+            #endregion
 
 
-            // Initialize BMP280 sensor
-            try
+            #region BMP280   
+            I2cDevice i2c_bmp280 = new(new I2cConnectionSettings(1, 0x76)); // Grove connector
+            var resBMP280 = i2c_tof.WriteByte(0x07);
+            if (resBMP280.Status == I2cTransferStatus.FullTransfer)
             {
-                I2cDevice i2c_bmp280 = I2cDevice.Create(new I2cConnectionSettings(2, 0x76));//on the same bus as sht40 -------beware: 0x76 is the address, not the val stored in the library 
-                bmp280 = new Bmp280(i2c_bmp280);
-                bmp280.SetPowerMode(Bmx280PowerMode.Normal);
-                bmp280.TemperatureSampling = Sampling.UltraHighResolution;
-                bmp280.PressureSampling = Sampling.UltraHighResolution;
+                sensorBMP280 = new Bmp280(i2c_bmp280);
+                if (sensorBMP280 != null)
+                {
+                    sensorBMP280.SetPowerMode(Bmx280PowerMode.Normal);
+                    sensorBMP280.TemperatureSampling = Sampling.UltraHighResolution;
+                    sensorBMP280.PressureSampling = Sampling.UltraHighResolution;
+                    sensorBMP280.TryReadPressure(out var bmpPressure);
+                    Debug.WriteLine($"sensorTOF: {bmpPressure.Hectopascals:F2}hPa");
+                }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Exception initializing BMP280: " + ex.ToString());
-            }
+            #endregion
+            #endregion
 
 
-            // Timer setup
-            Timer pubTimer = new Timer((s) => FireTelemetryData(), null, 0, pub_period);
 
+
+            #region LoRa E22           
             Configuration.SetPinFunction(pinCOM2_TX, DeviceFunction.COM2_TX);
             Configuration.SetPinFunction(pinCOM2_RX, DeviceFunction.COM2_RX);
             lora = E22.Create("COM2", loraAddress: loraAddress);
@@ -131,21 +172,15 @@ namespace CanSat
                 lora.OnPacketReceived += (sender, e) =>
                 {
                     Debug.WriteLine(e.Data.ToString());
-                    Blink.Blinks(0, 255, 0, 500, 1, 1);                    //Debug.WriteLine($"nFmem_LoRaRcv={Memory.Run(true)}");
-                    /*if (e.Data.ToString().StartsWith("6C6F6F706261636B"))    // loopback
-                    {
-                        int counter = Interlocked.Increment(ref loopback_counter);
-                        lora.SendAsync(e.AddressID, $"Loopback_#{counter:D4}", loraNetworkId);
-                    }*/
-                    //WriteTextOnScreen($"E22: {e.AddressID:X4} {e.Data}", X: 0, Y: LCD_Y_StatusLine, fcolor: Color.GreenYellow, maxChars: LCD_NumberOfSmallChars, font: SmallFont);
+                    // Blink.Blinks(0, 255, 0, 500, 1, 1);  // it must be async call                 
                 };
-                //sensors.Append("LoRa ");-----prida
             }
             #endregion
 
 
 
 
+            Timer pubTimer = new Timer((s) => FireTelemetryData(), null, 0, pub_period);
 
             // Button handler/callback
             buttonM5.Press += (sender, e) =>
@@ -160,25 +195,77 @@ namespace CanSat
             void FireTelemetryData()
             {
 
-                if (temp_hum_meter_snsr == null || sensorToF == null || bmp280 == null)
-                {
-                    Debug.WriteLine("Sensors are not initialized.");
-                    return;
-                }
-                // the place to put the code for handling an event from the pubTimer
-                Sht4XSensorData data = temp_hum_meter_snsr.ReadData(Iot.Device.Sht4x.MeasurementMode.NoHeaterHighPrecision);
-                var dist = sensorToF.GetDistanceOnce();
-                bmp280.TryReadPressure(out var bmpPressure);
+                Debug.WriteLine("");
+                Debug.WriteLine($"nFmem_FireStart={Memory.Run(true)}");
 
-                lora.Send(65535, "T" + Math.Floor( data.Temperature.DegreesCelsius ) + "H" + Math.Floor(data.RelativeHumidity.Percent) + "D" + dist + "P" + Math.Floor(bmpPressure.Hectopascals));
-                Blink.Blinks(0, 0, 255, 100, 1, 1);
+                try
+                {
+                    int counter = Interlocked.Increment(ref pub_counter);
+                    string payload = $"#{counter}";
+                    //SetLedByColor(0, 0, 2, 50);
+
+                    if (sensorTH != null)
+                    {
+                        var data = sensorTH.ReadData(Iot.Device.Sht4x.MeasurementMode.NoHeaterMediumPrecision);
+                        if (data != null)
+                        {
+                            var temperature = data.Temperature.DegreesCelsius;
+                            var humidity = data.RelativeHumidity.Percent;
+                            payload += $"T{temperature:F2}H{humidity:F2}";
+                        }
+                    }
+                    if (sensorToF != null)
+                    {
+                        payload += $"D{sensorToF.Distance}";
+                    }
+                    if (sensorBMP280 != null)
+                    {
+                        sensorBMP280.TryReadPressure(out var bmpPressure);
+                        payload += $"P{bmpPressure.Hectopascals:F2}";
+                    }
+
+                    // add more sensors to the payload    
+
+                    //
+                    Debug.WriteLine($">>> [{DateTime.UtcNow.ToString("hh:mm:ss.fff")}] {payload}");
+                    //
+                    if (lora != null && lora.IsOpen)
+                    {
+                        lora.SendAsync(address: BroadcastAddress, data: payload);
+                    }
+                    else
+                        Debug.WriteLine($"Device is not ready to publish message");
+
+                    //SetLedByColor(Color.Black);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.InnerException?.Message ?? ex.Message);
+                    //SetLedByColor(32, 0, 0);
+                }
+                finally
+                {
+                    Debug.WriteLine($"nFmem_FireEnd={Memory.Run(true)}");
+                    Debug.WriteLine("");
+                }
             }
 
+            #region Ready for IoT (last step)
+            Diag.PrintMemory("READY", true);
+            //timer_watchdog?.Dispose();
+            Blink.Blinks(0, 20, 0, 250, 2);      // good light
             Thread.Sleep(Timeout.Infinite);
-
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
+            #endregion
+            
+        }
+    }
+    public static class Diag
+    {
+        public static void PrintMemory(string msg, bool compactHeap = false)
+        {
+            NativeMemory.GetMemoryInfo(NativeMemory.MemoryType.All, out uint totalSize, out uint totalFreeSize, out uint largestBlock);
+            Debug.WriteLine($"{msg}-> Total Mem {totalSize} Total Free {totalFreeSize} Largest Block {largestBlock}");
+            Debug.WriteLine($"nF Mem {Memory.Run(compactHeap)} ");
         }
     }
 }
