d16b5e20ffb81b1302e4cf6bcd1e4b18cde1d2d9
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2025-03-27 19:20:00 +0100

    Update usage.cs
    Usage of the GPS Reader

--- Projects/2024/PatternsAndPractices/GPS/usage.cs
+++ Projects/2024/PatternsAndPractices/GPS/usage.cs
@@ -1,1 +1,39 @@
+// usage of the GPS reader
+
+using NFAppGPS;
+
+static GPS sensorGPS = null;
+
+
+#region GPS
+Configuration.SetPinFunction(pinCOM1_RX, DeviceFunction.COM1_RX);
+Configuration.SetPinFunction(pinCOM1_TX, DeviceFunction.COM1_TX);
+sensorGPS = GPS.Create("COM1", 9600);
+if (sensorGPS != null)
+{
+    bool isValid = sensorGPS.TryParseGNGGA(out float lat, out float lon, out float alt);
+    sensorGPS.OnGpsReceived_Unknown += (s, e) =>
+    {
+        //Debug.WriteLine(e.data);
+    };
+}
+#endregion
+
+
+
+private static void FireTelemetryData()
+{
+      ...
+        
+      //GPS
+      bool isValidGPS = false;
+      if (sensorGPS != null && sensorGPS.IsOpen)
+      {
+          isValidGPS = sensorGPS.TryParseGNGGA(out float lat, out float lon, out float alt);
+      }
+
+      ...  
+}
+
+
 
