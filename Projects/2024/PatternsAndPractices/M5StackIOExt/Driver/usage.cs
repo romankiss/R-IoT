49665d717408aab2980dbe487ff9ebf8dbea0176
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2025-04-09 11:03:53 +0200

    Update usage.cs
    Usage of the M5Stack IO extender via the I2C Bus

--- Projects/2024/PatternsAndPractices/M5StackIOExt/Driver/usage.cs
+++ Projects/2024/PatternsAndPractices/M5StackIOExt/Driver/usage.cs
@@ -1,1 +1,35 @@
+// usage of the driver for IO Extensions via I2C Bus
+
+static M5_EXTIO2 sensorIOExt = null;
+
+
+
+I2cDevice i2c_ioext = new(new I2cConnectionSettings(1, M5_EXTIO2.DefaultI2cAddress)); // Grove connector
+res = i2c_ioext.WriteByte(0x07);
+if (res.Status == I2cTransferStatus.FullTransfer)
+{
+    sensorIOExt = new M5_EXTIO2(i2c_ioext, ExtioIoMode.DigitalInputMode);    // all pins are inputs
+    sensorIOExt.SetPinMode(3, ExtioIoMode.DigitalOutputMode);                // reconfigure the pin#3 for output
+    sensorIOExt.SetPinMode(0, ExtioIoMode.RgbLedMode);                       // reconfigure the pin#0 for RGBLed (Neopixel)
+    sensorIOExt.SetLEDColor(0, (uint)Color.Blue.ToArgb());                   // set Neopixel#0 to Blue color
+}
+
+
+
+
+private static void FireTelemetryData()
+{
+   // ...
+
+   if (sensorIOExt != null)
+   {
+       // control output#3 based on the input#1
+       sensorIOExt?.SetDigitalOutput(3, sensorIOExt.GetDigitalInput(1));
+   }
+  
+   // ...
+}
+
+
+
 
