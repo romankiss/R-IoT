3f853aa60e40d26a59e862b20c22d9d2de0a1f3f
Marek-Maker <ratzenbock.marek@gmail.com>
2025-04-11 08:54:41 +0200

    Working DB upload, check correct connection string

--- Projects/2024/R-IoT-Data_Hub/Solutions/Basic/GroundStation/Remake of GND receiver/DatabaseManager.cs
+++ Projects/2024/R-IoT-Data_Hub/Solutions/Basic/GroundStation/Remake of GND receiver/DatabaseManager.cs
@@ -1,4 +1,5 @@
 ﻿using Npgsql;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 public class DatabaseManager
@@ -20,12 +21,15 @@ public class DatabaseManager
         {
             await using var connection = new NpgsqlConnection(_connectionString);
             await connection.OpenAsync();
-
+            Debug.WriteLine("Connected to DB successfully");
             await using var cmd = new NpgsqlCommand(query, connection);
             cmd.Parameters.AddWithValue("@devid", int.Parse(telemetryData["I"]));
-            cmd.Parameters.AddWithValue("@temperature", float.Parse(telemetryData["T"]));
-            cmd.Parameters.AddWithValue("@humidity", float.Parse(telemetryData["H"]));
-            cmd.Parameters.AddWithValue("@pressure", float.Parse(telemetryData["P"]));
+            telemetryData["T"] = telemetryData["T"].Replace(".", ","); // Replace dot with comma for decimal separator
+            telemetryData["H"] = telemetryData["H"].Replace(".", ","); // Replace dot with comma for decimal separator
+            telemetryData["P"] = telemetryData["P"].Replace(".", ","); // Replace dot with comma for decimal separator
+            cmd.Parameters.AddWithValue("@humidity", double.Parse(telemetryData["H"]));
+            cmd.Parameters.AddWithValue("@temperature", double.Parse(telemetryData["T"])); 
+            cmd.Parameters.AddWithValue("@pressure", double.Parse(telemetryData["P"]));
             cmd.Parameters.AddWithValue("@distance", int.Parse(telemetryData["D"]));
             cmd.Parameters.AddWithValue("@counter", int.Parse(telemetryData["#"]));
             //WARNING: DB has set acceptable ranges for some vals like humidity(0-100 & NULL), so passing it a -1 signing in our local environment, that there was an err reading hum (defacto NULL), will caouse an err in DB INSERT
