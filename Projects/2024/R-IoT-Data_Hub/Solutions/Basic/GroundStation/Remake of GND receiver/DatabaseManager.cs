e416504968b6b35e10513df495134e79d5b9db5e
Marek-Maker <ratzenbock.marek@gmail.com>
2025-04-11 10:06:07 +0200

    adding lat, long, alt to DB upload

--- Projects/2024/R-IoT-Data_Hub/Solutions/Basic/GroundStation/Remake of GND receiver/DatabaseManager.cs
+++ Projects/2024/R-IoT-Data_Hub/Solutions/Basic/GroundStation/Remake of GND receiver/DatabaseManager.cs
@@ -14,8 +14,8 @@ public class DatabaseManager
     public async Task SaveTelemetryAsync(Dictionary<string, string> telemetryData)
     {
         const string query = @"
-            INSERT INTO telemetry.measurements (devid, temperature, humidity, pressure, distance, counter)
-            VALUES (@devid, @temperature, @humidity, @pressure, @distance, @counter)";//WARNING: DevID is hardcoded to 1, change this to a variable if needed later
+            INSERT INTO telemetry.measurements (devid, temperature, humidity, pressure, distance, counter, latitude, longitude, altitude)
+            VALUES (@devid, @temperature, @humidity, @pressure, @distance, @counter, @latitude, @longitude, @altitude)";
 
         try
         {
@@ -27,11 +27,17 @@ public class DatabaseManager
             telemetryData["T"] = telemetryData["T"].Replace(".", ","); // Replace dot with comma for decimal separator
             telemetryData["H"] = telemetryData["H"].Replace(".", ","); // Replace dot with comma for decimal separator
             telemetryData["P"] = telemetryData["P"].Replace(".", ","); // Replace dot with comma for decimal separator
+            telemetryData["X"] = telemetryData["X"].Replace(".", ","); // Replace dot with comma for decimal separator
+            telemetryData["Y"] = telemetryData["Y"].Replace(".", ","); // Replace dot with comma for decimal separator
+            telemetryData["Z"] = telemetryData["Z"].Replace(".", ","); // Replace dot with comma for decimal separator
             cmd.Parameters.AddWithValue("@humidity", double.Parse(telemetryData["H"]));
             cmd.Parameters.AddWithValue("@temperature", double.Parse(telemetryData["T"])); 
             cmd.Parameters.AddWithValue("@pressure", double.Parse(telemetryData["P"]));
             cmd.Parameters.AddWithValue("@distance", int.Parse(telemetryData["D"]));
             cmd.Parameters.AddWithValue("@counter", int.Parse(telemetryData["#"]));
+            cmd.Parameters.AddWithValue("@latitude", double.Parse(telemetryData["X"]));
+            cmd.Parameters.AddWithValue("@longitude", double.Parse(telemetryData["Y"]));
+            cmd.Parameters.AddWithValue("@altitude", double.Parse(telemetryData["Z"]));
             //WARNING: DB has set acceptable ranges for some vals like humidity(0-100 & NULL), so passing it a -1 signing in our local environment, that there was an err reading hum (defacto NULL), will caouse an err in DB INSERT
             await cmd.ExecuteNonQueryAsync();
         }
